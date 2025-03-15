1ca70badee45f1032ab14d963d53c3668a4d8c29
Danilo <danilo.saraiva68@gmail.com>
2025-03-15 20:52:58 -0300

    chore(dotenv): add package dotenv

--- public/index.php
+++ public/index.php
@@ -2,6 +2,11 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 
 $app->dispath();
