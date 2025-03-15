e9bfc1094c49da070a174a231f518f3067c3ddc8
Danilo <danilo.saraiva68@gmail.com>
2025-03-15 20:53:38 -0300

    feat(config): file for configure modules

--- bootstrap/config.php
+++ bootstrap/config.php
@@ -2,6 +2,8 @@
 
 namespace Bootstrap;
 
+use Bootstrap\Modules\Guard\Guard;
+use Bootstrap\Modules\HandleError\HandleError;
 use Bootstrap\Modules\Middleware\Middleware;
 use Bootstrap\Modules\Request\Request;
 use Bootstrap\Modules\Response\Response;
@@ -10,12 +12,21 @@ use Bootstrap\Modules\Routing\Routing;
 return [
     'request'     => Request::class,
     'response'    => Response::class,
+    'handleError' => [
+        'module'   => HandleError::class,
+        'messages' => __DIR__ . '/../config/error.php',
+    ],
+
     'middleware'  => [
         'module'  => Middleware::class,
-        'stacker' => __DIR__ . '/../middleware.php',
+        'stacker' => __DIR__ . '/../config/middleware.php',
+    ],
+    'guard'  => [
+        'module'  => Guard::class,
+        'stacker' => __DIR__ . '/../config/guard.php',
     ],
     'routing'  => [
         'module'      => Routing::class,
-        'routes'      => __DIR__ . '/../router.php',
+        'routes'      => __DIR__ . '/../config/router.php',
     ],
 ];
