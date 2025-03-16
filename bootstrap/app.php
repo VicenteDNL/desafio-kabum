9d347d3786d24cf4d3e0a1f5cbe522383e7a6dcd
Danilo <danilo.saraiva68@gmail.com>
2025-03-15 21:00:00 -0300

    feat(Application): create dispath application

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,31 +2,41 @@
 
 namespace Bootstrap;
 
+use Bootstrap\Contracts\Guard;
+use Bootstrap\Contracts\HandleError;
+use Bootstrap\Contracts\Middleware;
 use Bootstrap\Contracts\Request;
 use Bootstrap\Contracts\Response;
 use Bootstrap\Contracts\Routing;
-use Bootstrap\Modules\Response\Response as ResponseResponse;
 use Exception;
+use Throwable;
 
 class Application
 {
     private static Application $instance;
     private Request $request;
+    private Response $response;
     private Routing $routing;
+    private Middleware $middleware;
+    private Guard $guard;
+    private HandleError $handleError;
     private array $config;
 
     private function __construct()
     {
         $this->config = require __DIR__ . '/config.php';
-
+        //TODO: alterar para injecao de dependencia
         $this->initRequest();
         $this->initRouting();
+        $this->initResponse();
+        $this->initMiddleware();
+        $this->initGuard();
+        $this->initHandleError();
 
     }
 
     public static function init()
     {
-
         if (!isset(self::$instance)) {
             self::$instance = new Application();
         }
@@ -34,14 +44,39 @@ class Application
         return self::$instance;
     }
 
-    public function dispath(): Response
+    public function dispath(): void
     {
-        $route = $this->routing->getRoute($this->request->getMethod(), $this->request->getPath());
-        $controller = new ($route->controller())($this->request);
-        $action = $route->action();
-        $result = $controller->$action();
+        try {
 
-        return new ResponseResponse();
+            $route = $this->routing->find($this->request);
+
+            $this->guard->start($this->request, $route);
+
+            $controller = new ($route->controller())($this->request);
+            $action = $route->action();
+            $handler = function () use ($controller, $action) {
+                return $controller->$action();
+            };
+
+            $result = $this->middleware->start($this->request, $handler);
+
+        } catch(Throwable $e) {
+
+            $result = $this->handleError->treat($this->request, $e);
+        }
+
+        if(is_object($result) && $result instanceof Response) {
+
+            header('Content-Type: ' . $result->getContentType());
+            http_response_code($result->getStatusCode());
+            echo $result->getContent();
+
+        } else {
+            $this->response->setContent($result);
+            header('Content-Type: ' . $this->response->getContentType());
+            http_response_code($this->response->getStatusCode());
+            echo $this->response->getContent();
+        }
 
     }
 
@@ -57,7 +92,7 @@ class Application
             throw new Exception('config["request"] is not instanceof ' . Request::class);
         }
 
-        $this->request = $request ;
+        $this->request = $request;
     }
 
     private function initRouting(): void
@@ -73,6 +108,70 @@ class Application
         }
 
         $this->routing = $routing ;
+    }
+
+    private function initResponse(): void
+    {
+        if(!class_exists($this->config['response'])) {
+            throw new Exception('config["response"] not configured');
+        }
+
+        //TODO: verificar seguranca do dado de httpaccept
+        $response = new ($this->config['response'])('', $this->request->getHttpAccept(), 200);
+
+        if(!($response instanceof Response)) {
+            throw new Exception('config["response"] is not instanceof ' . Response::class);
+        }
+
+        $this->response = $response ;
+    }
+
+    private function initMiddleware()
+    {
+        if(!class_exists($this->config['middleware']['module'])) {
+            throw new Exception('config["middleware"] not configured');
+        }
+
+        $middleware = $this->config['middleware']['module']::init($this->config['middleware']['stacker']);
+
+        if(!($middleware instanceof Middleware)) {
+            throw new Exception('config["middleware"]["module"] is not instanceof ' . Middleware::class);
+        }
+
+        $this->middleware = $middleware;
+
+    }
+
+    private function initGuard()
+    {
+        if(!class_exists($this->config['guard']['module'])) {
+            throw new Exception('config["guard"] not configured');
+        }
+
+        $guard = $this->config['guard']['module']::init($this->config['guard']['stacker']);
+
+        if(!($guard instanceof Guard)) {
+            throw new Exception('config["guard"]["module"] is not instanceof ' . Guard::class);
+        }
+
+        $this->guard = $guard;
+
+    }
+
+    private function initHandleError()
+    {
+        if(!class_exists($this->config['handleError']['module'])) {
+            throw new Exception('config["handleError"]["module"] not configured');
+        }
+
+        $handleError = $this->config['handleError']['module']::init($this->config['handleError']['messages']);
+
+        if(!($handleError instanceof HandleError)) {
+            throw new Exception('config["handleError"]["module"] is not instanceof ' . HandleError::class);
+        }
+
+        $this->handleError = $handleError;
+
     }
 }
 
