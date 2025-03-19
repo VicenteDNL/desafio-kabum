b09b457c9449e76bf56941f48f4cb9a795c7b143
Danilo <danilo.saraiva68@gmail.com>
2025-03-18 21:37:12 -0300

    refactor(cors): add configuration in env

--- public/index.php
+++ public/index.php
@@ -1,13 +1,13 @@
 <?php
 
-header('Access-Control-Allow-Origin: http://localhost:3000');
-
 require __DIR__ . '/../vendor/autoload.php';
 
 use Dotenv\Dotenv;
 
 $dotenv = Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
+
+header('Access-Control-Allow-Origin: ' . ($_ENV['CORS_ORIGIN'] ?? '*'));
 
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 
